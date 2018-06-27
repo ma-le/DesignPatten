bcaf859135ccc9bef9cc3d1aad747fc57333411b
mal <mal@hui10.com>
2018-06-27 17:16:31 +0800

    观察者模式

---
+++ src/com/male/ObserverPatten/AccountObserver.java
@@ -0,0 +1,18 @@
+package com.male.ObserverPatten;
+
+/**
+ * 出账
+ */
+public class AccountObserver extends Observer {
+
+	public AccountObserver(Subject subject){
+		this.subject = subject;
+		this.subject.add(this);
+	}
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		System.out.println("利率有变化   对应出账☆☆☆☆☆要变更  此处做具体业务   利率变为:"+subject.getMoneyRate());
+	}
+
+}
---
+++ src/com/male/ObserverPatten/Observer.java
@@ -0,0 +1,7 @@
+package com.male.ObserverPatten;
+
+public abstract class Observer {
+
+	protected Subject subject;
+	public abstract void update();
+}
